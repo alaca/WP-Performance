4c2fa88de6558cd7e30ab84634969e2575271ef2
alaca <ante.laca@gmail.com>
2019-01-01 10:48:13 +0100

    Check if element exists

--- assets/cache.js
+++ assets/cache.js
@@ -2,21 +2,25 @@ window.addEventListener('DOMContentLoaded', function() {
 
     var clear_cache = document.querySelector('li.wpp_clear_cache a');
 
-    clear_cache.addEventListener('click', function(e) {
+    if ( null !== clear_cache ) {
 
-        e.preventDefault();
+        clear_cache.addEventListener('click', function(e) {
 
-        document.body.innerHTML += '<div id="wpp_overlay"><img id="wpp_loader" src="' + WPP.path + 'loader.svg" /></div>';
+            e.preventDefault();
+    
+            document.body.innerHTML += '<div id="wpp_overlay"><img id="wpp_loader" src="' + WPP.path + 'loader.svg" /></div>';
+    
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', WPP.ajax_url, true);
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            xhr.send('action=wpp_clear_cache');
+            xhr.onload = function(){
+                var overlay = document.getElementById('wpp_overlay');
+                overlay.parentNode.removeChild(overlay);
+            };
+    
+        }, true );
 
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', WPP.ajax_url, true);
-        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        xhr.send('action=wpp_clear_cache');
-        xhr.onload = function(){
-            var overlay = document.getElementById('wpp_overlay');
-            overlay.parentNode.removeChild(overlay);
-        };
-
-    }, true );
+    }
 
 });
