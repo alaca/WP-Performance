2bd546cd27957d2b5096db7085bc16759985a31e
alaca <ante.laca@gmail.com>
2019-01-10 14:20:02 +0100

    Add database cleanup commands

--- includes/classes/cli.php
+++ includes/classes/cli.php
@@ -22,7 +22,7 @@ class CLI extends WP_CLI_Command
 
 		WP_CLI::line( esc_html__( 'Flushing the cache...', 'wpp' ) );
 		Cache::clear( false );
-        WP_CLI::success( esc_html__( 'Cache flushed.', 'wpp' ) );
+        WP_CLI::line( esc_html__( 'Cache flushed.', 'wpp' ) );
         
 	}
 
@@ -36,7 +36,7 @@ class CLI extends WP_CLI_Command
 
 		WP_CLI::line( esc_html__( sprintf( 'Disabling %s...', WPP_PLUGIN_NAME ), 'wpp' ) );
 		Option::update( 'wpp_disable', true );
-        WP_CLI::success( esc_html__( sprintf( '%s disabled', WPP_PLUGIN_NAME ), 'wpp' ) );
+        WP_CLI::line( esc_html__( sprintf( '%s disabled', WPP_PLUGIN_NAME ), 'wpp' ) );
         
 	}
 
@@ -50,7 +50,82 @@ class CLI extends WP_CLI_Command
 
 		WP_CLI::line( esc_html__( sprintf( 'Enabling %s...', WPP_PLUGIN_NAME ), 'wpp' ) );
 		Option::update( 'wpp_disable', false );
-        WP_CLI::success( esc_html__( sprintf( '%s enabled', WPP_PLUGIN_NAME ), 'wpp' ) );
+        WP_CLI::line( esc_html__( sprintf( '%s enabled', WPP_PLUGIN_NAME ), 'wpp' ) );
+        
+	}
+
+	/**
+	 * Cleanup database
+	 *
+	 * @since 1.0.5 
+	 * @return void
+	 */
+	public function cleanup( $args ) {
+
+		if ( isset( $args[ 0 ] ) ) {
+
+			switch( $args[ 0 ] ) {
+
+				case 'transients':
+
+					WP_CLI::line( esc_html__( 'Running tranisent cleanup', 'wpp' ) );
+					DB::clearTransients();
+					WP_CLI::line( esc_html__( 'Transient cleanup complete', 'wpp' ) );
+
+					break;
+
+				case 'revisions':
+
+					WP_CLI::line( esc_html__( 'Running revisions cleanup', 'wpp' ) );
+					DB::clearRevisions();
+					WP_CLI::line( esc_html__( 'Revisions cleanup complete', 'wpp' ) );
+
+					break;
+
+				case 'drafts':
+
+					WP_CLI::line( esc_html__( 'Running drafts cleanup', 'wpp' ) );
+					DB::clearDrafts();
+					WP_CLI::line( esc_html__( 'Drafts cleanup complete', 'wpp' ) );
+
+					break;
+
+				case 'trash':
+
+					WP_CLI::line( esc_html__( 'Running trash cleanup', 'wpp' ) );
+					DB::clearTrash();
+					WP_CLI::line( esc_html__( 'Trash cleanup complete', 'wpp' ) );
+
+					break;
+
+				case 'spam':
+
+					WP_CLI::line( esc_html__( 'Running spam cleanup', 'wpp' ) );
+					DB::clearSpam();
+					WP_CLI::line( esc_html__( 'Spam cleanup complete', 'wpp' ) );
+
+					break;
+
+				case 'cron':
+
+					WP_CLI::line( esc_html__( 'Running cron tasks cleanup', 'wpp' ) );
+					DB::clearCronTasks();
+					WP_CLI::line( esc_html__( 'Cron tasks cleanup complete', 'wpp' ) );
+
+					break;
+
+			}
+
+		} else {
+
+			/**
+			 * Clear all
+			 */
+			WP_CLI::line( esc_html__( 'Running database cleanup', 'wpp' ) );
+			DB::clear();
+			WP_CLI::line( esc_html__( 'Database cleanup complete', 'wpp' ) );
+
+		}
         
 	}
 
