0b895263f3daf7b3d2d459ac2259798437bf14bf
alaca <ante.laca@gmail.com>
2019-01-13 13:28:41 +0100

    Fix clear cache bug and include nonce parameter

--- assets/cache.js
+++ assets/cache.js
@@ -6,14 +6,12 @@ window.addEventListener('DOMContentLoaded', function() {
 
         clear_cache.addEventListener('click', function(e) {
 
-            e.preventDefault();
-    
-            document.body.innerHTML += '<div id="wpp_overlay"><img id="wpp_loader" src="' + WPP.path + 'loader.svg" /></div>';
+            document.body.insertAdjacentHTML('beforeend', '<div id="wpp_overlay"><img id="wpp_loader" src="' + WPP.path + 'loader.svg" /></div>' );
     
             var xhr = new XMLHttpRequest();
             xhr.open('POST', WPP.ajax_url, true);
             xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-            xhr.send('action=wpp_clear_cache');
+            xhr.send('action=wpp_clear_cache&nonce=' + WPP.nonce);
             xhr.onload = function(){
                 var overlay = document.getElementById('wpp_overlay');
                 overlay.parentNode.removeChild(overlay);
