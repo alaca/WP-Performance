a6cecd7c3519b60b80cb0a93bafb072b93f4626d
alaca <ante.laca@gmail.com>
2019-01-30 17:57:58 +0100

    Update version; Define WPP_ADDONS_DIR constant.

--- wp-performance.php
+++ wp-performance.php
@@ -3,7 +3,7 @@
 * Plugin Name: WP Performance
 * Plugin URI: https://www.wp-performance.com
 * Description: WP Performance Optimizer
-* Version: 1.0.9
+* Version: 1.1.0
 * Author: Ante Laca
 * Author URI: https://www.antelaca.xyz
 * Licence: GPLv2
@@ -14,18 +14,22 @@
 defined( 'ABSPATH' ) or exit;
 
 // WP Performance
-define( 'WPP_VERSION'       , '1.0.9' );
+define( 'WPP_VERSION'       , '1.1.0' );
 define( 'WPP_SELF'          , __FILE__ );
 define( 'WPP_URI'           , plugin_dir_url( __FILE__ ) );
 define( 'WPP_DIR'           , plugin_dir_path( __FILE__ ) ); 
 define( 'WPP_ASSET_DIR'     , trailingslashit( WPP_DIR )        . 'assets/' );
 define( 'WPP_ASSET_URL'     , trailingslashit( WPP_URI )        . 'assets/' );
 define( 'WPP_CLASSES_DIR'   , trailingslashit( WPP_DIR )        . 'includes/classes/' );
+define( 'WPP_ADDONS_DIR'    , trailingslashit( WPP_DIR )        . 'includes/addons/' );
 define( 'WPP_FUNCTIONS_DIR' , trailingslashit( WPP_DIR )        . 'includes/functions/' );
 define( 'WPP_ADMIN_DIR'     , trailingslashit( WPP_DIR )        . 'includes/admin/' );
 define( 'WPP_DATA_DIR'      , trailingslashit( WPP_DIR )        . 'includes/data/' );
 define( 'WPP_CACHE_DIR'     , trailingslashit( WP_CONTENT_DIR ) . 'cache/wpp-cache/' );
-define( 'WPP_CACHE_URL'     , trailingslashit( WP_CONTENT_URL ) . 'cache/wpp-cache/' ); 
+define( 'WPP_CACHE_URL'     , trailingslashit( WP_CONTENT_URL ) . 'cache/wpp-cache/' );
+// Define plugin name and plugin admin url
+define( 'WPP_PLUGIN_NAME'     , 'WP Performance' );
+define( 'WPP_PLUGIN_ADMIN_URL', sanitize_title( WPP_PLUGIN_NAME ) );
 
 require WPP_CLASSES_DIR . 'wpp.php';
 
