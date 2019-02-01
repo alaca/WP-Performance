a252945ca7a2f548297acf82371384b96d3bfa5d
alaca <ante.laca@gmail.com>
2019-02-01 11:13:14 +0100

    Add logo and additional options

--- includes/addons/varnish/views/addon.php
+++ includes/addons/varnish/views/addon.php
@@ -4,25 +4,51 @@ defined('ABSPATH') or exit; ?>
 
 <div class="wpp-addon">
 
-    <?php if ( Option::boolval( 'cf_enabled' ) ): ?>
+    <div class="wpp-addon-content">
 
-        <a href="#" data-wpp-show-page="cloudflare" class="wpp-addon-settings-link">
-            <i class="dashicons dashicons-admin-generic"></i>
-        </a>
+        <?php if ( Option::boolval( 'varnish_auto_purge' ) ): ?>
 
-    <?php endif; ?>
+            <a href="#" data-addon-settings="varnish" class="wpp-addon-settings-link wpp-addon-settings-toggle">
+                <i class="dashicons dashicons-admin-generic"></i>
+            </a>
 
-    <strong><?php _e( 'Cloudflare', 'wpp' ); ?></strong>
+        <?php endif; ?>
 
-    <div>
-        <?php _e( 'Integrate your Cloudflare account with this add-on.', 'wpp' ); ?>
+        <img src="<?php echo WPP_ADDONS_URL ?>varnish/assets/varnish.png">
+
+        <div>
+            <?php _e( 'If Varnish runs on your server, you must activate this add-on.', 'wpp' ); ?>
+        </div>
+
+        <br />
+
+        <label class="wpp-info">
+            <input type="checkbox" value="1" data-wpp-checkbox="varnish_auto_purge" value="1" name="varnish_auto_purge" form="wpp-settings" <?php wpp_checked( 'varnish_auto_purge' ); ?> />
+            <?php _e( 'Enable', 'wpp' ); ?> Varnish
+        </label>
+
+        <div class="wpp-addon-settings" data-addon-settings-content="varnish">
+
+            <hr />
+
+            <p>
+                <?php _e( 'Custom host', 'wpp' ); ?>
+            </p>
+
+            <input 
+                type="text" 
+                placeholder="http://"
+                name="varnish_custom_host" 
+                value="<?php echo Option::get( 'varnish_custom_host' ); ?>" 
+                form="wpp-settings"
+                class="wpp-addon-input" />  
+                    
+                <br /><br />
+
+            <em><span class="dashicons dashicons-info"></span> <?php _e( 'If you are using proxy, you may need this option', 'wpp' ); ?></em>
+
+        </div>
+
     </div>
-
-    <br />
-
-    <label class="wpp-info">
-        <input type="checkbox" value="1" name="cf_enabled" form="wpp-settings" <?php wpp_checked( 'cf_enabled' ); ?> />
-        <?php _e( 'Enable', 'wpp' ); ?> Cloudflare
-    </label>
 
 </div>
