d338ba7bb5cf0fa445f380ff7bf3006a4d9b0d83
alaca <ante.laca@gmail.com>
2019-02-01 11:13:00 +0100

    Add  logo

--- includes/addons/cloudflare/views/addon.php
+++ includes/addons/cloudflare/views/addon.php
@@ -4,25 +4,29 @@ defined('ABSPATH') or exit; ?>
 
 <div class="wpp-addon">
 
-    <?php if ( Option::boolval( 'cf_enabled' ) ): ?>
+    <div class="wpp-addon-content">
 
-        <a href="#" data-wpp-show-page="cloudflare" class="wpp-addon-settings-link button">
-            <?php _e( 'Settings', 'wpp' ); ?>
-        </a>
+        <?php if ( Option::boolval( 'cf_enabled' ) ): ?>
 
-    <?php endif; ?>
+            <a href="#" data-wpp-show-page="cloudflare" class="wpp-addon-settings-link">
+                <i class="dashicons dashicons-admin-generic"></i>
+            </a>
 
-    <strong><?php _e( 'Cloudflare', 'wpp' ); ?></strong>
+        <?php endif; ?>
 
-    <div>
-        <?php _e( 'Integrate your Cloudflare account with this add-on.', 'wpp' ); ?>
+        <img src="<?php echo WPP_ADDONS_URL ?>cloudflare/assets/cf-logo-h.svg">
+
+        <div>
+            <?php _e( 'Integrate your Cloudflare account with this add-on.', 'wpp' ); ?>
+        </div>
+
+        <br />
+
+        <label class="wpp-info">
+            <input type="checkbox" value="1" name="cf_enabled" form="wpp-settings" <?php wpp_checked( 'cf_enabled' ); ?> />
+            <?php _e( 'Enable', 'wpp' ); ?> Cloudflare
+        </label>
+
     </div>
-
-    <br />
-
-    <label class="wpp-info">
-        <input type="checkbox" value="1" name="cf_enabled" form="wpp-settings" <?php wpp_checked( 'cf_enabled' ); ?> />
-        <?php _e( 'Enable', 'wpp' ); ?> Cloudflare
-    </label>
 
 </div>
