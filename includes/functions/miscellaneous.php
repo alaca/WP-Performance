d81936a20f9f3d48b2d3ea634597af1498cb4d0e
alaca <ante.laca@gmail.com>
2019-07-02 14:28:55 +0200

    Add disable emoji and disable embeds functions

--- includes/functions/miscellaneous.php
+++ includes/functions/miscellaneous.php
@@ -9,6 +9,7 @@
 use WPP\Url;
 use WPP\File;
 use WPP\Cache;
+use WPP\Input;
 use WPP\Option;
 
 /**
@@ -127,13 +128,112 @@ function wpp_cleanup_header() {
     remove_action( 'wp_head', 'rsd_link' );       
     remove_action( 'wp_head', 'wp_shortlink_wp_head' );
     remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10 );    
-    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-    remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
     add_filter( 'the_generator', '__return_false' ); 
 
 }
 
+
+/**
+ * Disable Emoji
+ * 
+ * @since 1.1.6
+ * @return void
+ */
+function wpp_disable_emoji() {
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    add_filter( 'tiny_mce_plugins', 'wpp_disable_emojis_tinymce' );
+    add_filter( 'wp_resource_hints', 'wpp_disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+
+/**
+ * Remove the tinymce emoji plugin
+ * 
+ * @since 1.1.6
+ * @param array $plugins 
+ * @return array
+ */
+function wpp_disable_emojis_tinymce( $plugins ) {
+
+    if ( is_array( $plugins ) )
+        return array_diff( $plugins, [ 'wpemoji' ] );
+
+    return [];
+}
+   
+/**
+* Remove emoji CDN hostname from DNS prefetching hints
+
+* @since 1.1.6
+* @param array $urls
+* @param string $relation
+* @return array 
+*/
+function wpp_disable_emojis_remove_dns_prefetch( $urls, $relation ) {
+
+    if ( 'dns-prefetch' == $relation ) {
+        $url  = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+        $urls = array_diff( $urls, [ $url ] );
+    }
+
+    return $urls;
+}
+
+
+/**
+ * Disable WP embeds
+ * 
+ * @since 1.1.6
+ * @return void
+ */
+function wpp_disable_embeds() {
+
+    remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+    remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+    remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+
+    remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+    remove_filter( 'pre_oembed_result', 'wp_filter_pre_oembed_result', 10 );
+
+    add_filter( 'embed_oembed_discover', '__return_false' );
+    add_filter( 'tiny_mce_plugins', 'wpp_disable_embeds_tinymce_plugin' );
+    add_filter( 'rewrite_rules_array', 'wpp_disable_embeds_rewrites' );
+
+}
+
+/**
+ * Remove the tinymce embed plugin
+ * 
+ * @since 1.1.6
+ * @param array $plugins 
+ * @return array
+ */
+function wpp_disable_embeds_tinymce_plugin( $plugins ) {
+    return array_diff( $plugins, [ 'wpembed' ] );
+}
+
+/**
+ * Disable embed rewrite rules
+ * 
+ * @since 1.1.6
+ * @param array $rules 
+ * @return array
+ */
+function wpp_disable_embeds_rewrites( $rules ) {
+
+    foreach ( $rules as $rule => $rewrite ) {
+        if (false !== strpos( $rewrite, 'embed=true' ) )
+            unset( $rules[ $rule ] );
+    }
+
+    return $rules;
+}
    
 
 /**
@@ -400,5 +500,25 @@ function wpp_get_constant( $constant, $fallback = false ) {
     return defined( $constant ) 
         ? constant( $constant ) 
         : $fallback;
+
+}
+
+/**
+ * Check if it's an ajax request
+ * @since 1.1.6
+ *
+ * @return bool
+ */
+function wpp_is_ajax() {
+
+    if( 
+        Input::server( 'HTTP_X_REQUESTED_WITH' ) 
+        && strtolower( Input::server( 'HTTP_X_REQUESTED_WITH' ) ) == 'xmlhttprequest' 
+        || ( function_exists( 'wp_doing_ajax' ) && wp_doing_ajax() )
+    ) {
+        return true;
+    }
+
+    return false;
 
 }
