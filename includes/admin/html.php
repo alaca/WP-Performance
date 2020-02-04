e58f874bc0fe89864237a54309c40619448191b6
alaca <ante.laca@gmail.com>
2020-02-04 22:04:18 +0100

    Add html page

---
+++ includes/admin/html.php
@@ -0,0 +1,30 @@
+<?php namespace WPP;
+
+defined('ABSPATH') or exit; ?>
+
+<div class="wpp-page-wrapper">
+
+    <div class="wpp-content-section">
+    
+
+
+        <br /><br />
+
+    </div>
+
+    <div class="wpp-side-section">
+    
+        <h3><?php _e('HTML settings', 'wpp'); ?></h3>
+        
+        <hr />       
+
+        <?php do_action( 'wpp-html-side-section-top' ); ?> 
+        
+        <br />
+
+        <?php do_action( 'wpp-html-side-section-bottom'); ?>
+        
+        
+    </div>
+
+</div>
